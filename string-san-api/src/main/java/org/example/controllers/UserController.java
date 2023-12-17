55324953ae840f6246985a0c05422635e50ebeb4
Elizabeth Harasymiw <Elizabeth.Harasymiw@gmail.com>
2023-12-16 21:16:47 -0600

    Created controllers package and first test route

---
+++ string-san-api/src/main/java/org/example/controllers/UserController.java
@@ -0,0 +1,15 @@
+package org.example.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "User: John Doe";
+    }
+
+}
