1c53207975244e3d0778c221ad03e8231de5b590
Elizabeth Harasymiw <Elizabeth.Harasymiw@gmail.com>
2023-12-17 22:42:38 -0600

    Modified /getUser to fetch a user from a userMap

--- string-san-api/src/main/java/org/example/controllers/UserController.java
+++ string-san-api/src/main/java/org/example/controllers/UserController.java
@@ -3,14 +3,25 @@ package org.example.controllers;
 
 import org.example.model.UserModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/getUser")
-    public UserModel getUser(){
-        return new UserModel("John", "Doe", 1234);
+    private Map<String, UserModel> userMap = new HashMap<>();
+
+    public UserController(){
+        userMap.put("John", new UserModel("John", "Doe", 1234));
+        userMap.put("Jane", new UserModel("Jane", "Doe", 5432));
+    }
+
+    @GetMapping("/getUser/{userName}")
+    public UserModel getUser(@PathVariable String userName){
+        return userMap.get(userName);
     }
 
 }
