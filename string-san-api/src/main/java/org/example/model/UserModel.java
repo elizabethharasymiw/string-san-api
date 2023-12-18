369a5a8b448fdaf4b97a4345238e10540ef002fb
Elizabeth Harasymiw <Elizabeth.Harasymiw@gmail.com>
2023-12-17 22:28:34 -0600

    Add model package and UserModel Class

---
+++ string-san-api/src/main/java/org/example/model/UserModel.java
@@ -0,0 +1,39 @@
+package org.example.model;
+
+public class UserModel {
+
+    private String firstName;
+    private String lastName;
+    private Integer membershipId;
+
+    public UserModel(String firstname, String lastName, Integer membershipId){
+        this.firstName = firstname;
+        this.lastName = lastName;
+        this.membershipId = membershipId;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public Integer getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(Integer membershipId) {
+        this.membershipId = membershipId;
+    }
+}
+
