d157d8e9a63828c8863fdad14afe039ac9447db4
Elizabeth Harasymiw <Elizabeth.Harasymiw@gmail.com>
2023-12-17 22:33:12 -0600

    Update /getUser request to return a UserModel instead of a String
    This will be serialized into Json by the springboot framework

--- string-san-api/src/main/java/org/example/controllers/UserController.java
+++ string-san-api/src/main/java/org/example/controllers/UserController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 
 
+import org.example.model.UserModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/getUser")
-    public String getUser(){
-        return "User: John Doe";
+    public UserModel getUser(){
+        return new UserModel("John", "Doe", 1234);
     }
 
 }
